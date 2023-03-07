b1bfce0f34cce4cd1a068521acb73bf4e9fc2160
Vinicius Costa Sato <v.alvescosta26@gmail.com>
2023-03-07 18:33:01 -0300

    Pagina padrão

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,30 @@
+import styles from './Menu.module.scss';
+import { ReactComponent as Logo } from 'Assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+export default function Menu() {
+  const rotas = [{
+    label: 'Início',
+    to: '/'
+  }, {
+    label: 'Cardápio',
+    to: '/cardapio'
+  }, {
+    label: 'Sobre',
+    to: '/sobre'
+  }];
+  return( 
+    <nav className={styles.menu}>
+      <Logo />
+      <ul className={styles.menu__list }>
+        {rotas.map((rotas,index) => (
+          <li key={index} className={styles.menu__link}>
+            <Link to={rotas.to}>
+              {rotas.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
+import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'Pages/Cardapio';
 import Inicio from 'Pages/Inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio />} />
-        <Route path='/cardapio' element={<Cardapio />} />
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
